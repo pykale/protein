48b7cd89f68696efdc07469a67fca153f7001334
wenruifan <wenrui.fan@sheffield.ac.uk>
2026-07-10 14:12:51 +0100

    Add reusable DTI dataset loaders

--- kale_protein/auto/auto_loader.py
+++ kale_protein/auto/auto_loader.py
@@ -1,11 +1,11 @@
 class AutoProteinData:
     """Registry-backed data entrypoint for readable demos and tests."""
 
-    def __new__(cls, data_id):
+    def __new__(cls, data_id, *args, **kwargs):
         import kale_protein  # noqa: F401 bootstrap registrations
         from kale_protein.registry import DATASET_REGISTRY
         loader = DATASET_REGISTRY.get(data_id)
-        return loader() if callable(loader) else loader
+        return loader(*args, **kwargs) if callable(loader) else loader
 
 
 class AutoProteinDataLoader:
